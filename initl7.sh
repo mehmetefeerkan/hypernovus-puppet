1247d92185f8494b77e166df00de2e26f299fbdd
mehmetefeerkan <69371881+mehmetefeerkan@users.noreply.github.com>
2021-05-20 15:26:57 +0300

    Update initl7.sh

--- initl7.sh
+++ initl7.sh
@@ -2,12 +2,18 @@
 
 JSURL=https://raw.githubusercontent.com/mehmetefeerkan/hypernovus-puppet/main/source/index.js
 PYURL=https://raw.githubusercontent.com/mehmetefeerkan/hypernovus-puppet/main/source/hulk.py
-RUNNERURL=
+
+RUNNERURL=https://raw.githubusercontent.com/mehmetefeerkan/hypernovus-puppet/main/runl7.sh
+CLEANUPURL=https://raw.githubusercontent.com/mehmetefeerkan/hypernovus-puppet/main/cleanupl7.sh
 
 apt install npm
 apt install nodejs
 
 wget $RUNNERURL
+chmod +x runl7.sh
+
+wget $CLEANUPURL
+chmod +x cleanupl7.sh
 
 mkdir l7flood
 cd l7flood
