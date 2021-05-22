e099af4690f7ac66b172a78f2f6c9110855efcba
Mehmet Efe Erkan <69371881+mehmetefeerkan@users.noreply.github.com>
2021-05-22 05:28:42 +0300

    Update indextest.js

--- source/indextest.js
+++ source/indextest.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { spawn } = require('child_process');
 const app = express()
-
+const port = 3000
 
 app.get('/layer7/:victim/:time', (req, res) => {
         let victim = req.params.victim
